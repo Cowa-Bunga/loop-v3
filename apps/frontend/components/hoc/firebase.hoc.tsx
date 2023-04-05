e01f54c1145e1033451dd1cfd4156dec6c405d1f
Reme Le Hane <reme@loop.co.za>
2023-04-05 13:07:15 +0200

    feat(auth): [LOOP] added HOC to wrap firebase provider

---
+++ apps/frontend/components/hoc/firebase.hoc.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { getFirestore } from 'firebase/firestore';
+import { FirestoreProvider, useFirebaseApp } from 'reactfire';
+
+const FirebaseHOC = (Component) => {
+  return function HOCWrapper(props) {
+    const firestoreInstance = getFirestore(useFirebaseApp());
+    return (
+      <FirestoreProvider sdk={firestoreInstance}>
+        <Component {...props} />
+      </FirestoreProvider>
+    );
+  };
+};
+export default FirebaseHOC;
