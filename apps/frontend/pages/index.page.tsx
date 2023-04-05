c31c83dde62196f7c4606630f2e87defc14a14ec
Reme Le Hane <reme@loop.co.za>
2023-04-05 13:14:48 +0200

    fix(locale): [LOOP] simplified logic to use router

--- apps/frontend/pages/index.page.tsx
+++ apps/frontend/pages/index.page.tsx
@@ -1,17 +1,7 @@
 import Dashboard from './dashboard/index.page';
-import { useSession } from 'next-auth/react';
-import SignIn from './auth/signin/index.page';
-import { FirestoreProvider, useFirebaseApp } from 'reactfire';
-import { getFirestore } from 'firebase/firestore';
 
 export function LoopFrontend() {
-  const { data: session } = useSession();
-  const firestoreInstance = getFirestore(useFirebaseApp());
-  return (
-    <FirestoreProvider sdk={firestoreInstance}>
-      {session ? <Dashboard /> : <SignIn />}
-    </FirestoreProvider>
-  );
+  return <Dashboard />;
 }
 
 export default LoopFrontend;
