34fd0c2049d70f62f86279b7e024348e5c34e990
timLycett-loop <128364653+timLycett-loop@users.noreply.github.com>
2023-04-06 20:43:19 +0200

    removed peer conflict test lib

--- apps/frontend/pages/company/kyc/index.page.tsx
+++ apps/frontend/pages/company/kyc/index.page.tsx
@@ -1,6 +1,7 @@
 import { LayoutBase } from '@components'
 
 // @see: https://react-jsonschema-form-material-ui-github56.vercel.app
+// @see: https://ginkgobioworks.github.io/react-json-schema-form-builder
 const CompanyKYC = () => {
   return <LayoutBase>CompanyKYC</LayoutBase>
 }
