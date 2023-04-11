e83fb69e8016225868b0bf4bf51f4fcf13d611d3
Reme Le Hane <reme@loop.co.za>
2023-04-11 08:44:38 +0200

    test(app): [LOOP] fixed failing tests

--- apps/frontend/__test__/test-helpers.tsx
+++ apps/frontend/__test__/test-helpers.tsx
@@ -1,24 +1,15 @@
-import { render } from '@testing-library/react';
-import { FirebaseAppProvider } from 'reactfire';
+import { render } from '@testing-library/react'
+import { FirebaseAppProvider } from 'reactfire'
+import { firebaseConfig } from '@util/lib/firebase'
 
-const firebaseConfigMock = {
-  apiKey: 'mock-api-key',
-  authDomain: 'mock-auth-domain',
-  projectId: 'mock-project-id',
-  storageBucket: 'mock-storage-bucket',
-  messagingSenderId: 'mock-messaging-sender-id',
-  appId: 'mock-app-id',
-  measurementId: 'mock-measurement-id',
-  experimentalForceLongPolling: true
-};
 export const renderWithProviders = (ui: JSX.Element) => {
   return {
     ...render(ui, {
       wrapper: ({ children }) => (
-        <FirebaseAppProvider firebaseConfig={firebaseConfigMock}>
+        <FirebaseAppProvider firebaseConfig={firebaseConfig}>
           {children}
         </FirebaseAppProvider>
       )
     })
-  };
-};
+  }
+}
