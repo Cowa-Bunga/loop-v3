27d79e87dd37c8071e33c37f52e69a2eaeebd336
Reme Le Hane <reme@loop.co.za>
2023-04-11 17:16:08 +0200

    fix(app): [LOOP] updated interface for user context

--- apps/frontend/util/types/IappContext.d.ts
+++ apps/frontend/util/types/IappContext.d.ts
@@ -1,4 +1,14 @@
+import { IMeHub, IMeOrganization } from '@pages/api/me/me.interface'
+
 interface IUserContext {
+  id: string
+  firstName: string
+  lastName: string
+  mobileNo: string
+  organization: IMeOrganization
+  permissions: IUserPermissions
+  regions: string[]
+  hubs: IMeHub[]
   client: IClient
   firebase_token: string
 }
