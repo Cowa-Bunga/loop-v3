78626cb0d5f4b9cc2fd3880e43bcaf8026457775
Reme Le Hane <reme@loop.co.za>
2023-04-12 09:50:00 +0200

    fix(app): [LOOP] implemented API handler

--- apps/frontend/pages/api/me/index.page.ts
+++ apps/frontend/pages/api/me/index.page.ts
@@ -1,24 +1,9 @@
-import axios, { AxiosResponse } from 'axios'
-import { getServerSession, NextAuthOptions } from 'next-auth'
-import { authOptions } from '@pages/api/auth/[...nextauth].page'
 import { IMeInterface } from '@pages/api/me/me.interface'
+import { handler } from '@pages/api/api.handler'
 
 export default async (req, res) => {
   try {
-    const session = await getServerSession(
-      req,
-      res,
-      authOptions as NextAuthOptions
-    )
-    const data = { ...session.user } as ISessionUser
-
-    const response: AxiosResponse = await axios<AxiosResponse<IMeInterface>>({
-      url: `${process.env.DOS_API_URL}/me`,
-      method: 'get',
-      headers: {
-        Authorization: `Bearer ${data.token}`
-      }
-    })
+    const response = await handler<IMeInterface>(req, res, 'me', 'GET')
 
     res.status(200).json(response.data)
   } catch (e) {
