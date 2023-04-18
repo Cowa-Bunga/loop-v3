ddd2f7cba10b6097f1916311b069a784674ee130
Reme Le Hane <reme@loop.co.za>
2023-04-18 14:49:04 +0200

    test(auth): [LOOP] setup initial cypress testing

--- apps/frontend-e2e/src/support/e2e.ts
+++ apps/frontend-e2e/src/support/e2e.ts
@@ -1,5 +1,5 @@
 // ***********************************************************
-// This example support/index.js is processed and
+// This example support/e2e.ts is processed and
 // loaded automatically before your test files.
 //
 // This is a great place to put global configuration and
@@ -15,3 +15,16 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // we expect a 3rd party library error with message 'list not defined'
+  // and don't want to fail the test so we return false
+  if (err.message.includes('list not defined')) {
+    return false
+  }
+  // we still want to ensure there are no other unexpected
+  // errors, so we let them fail the test
+})
