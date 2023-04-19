a71fa469970139455c5720eed96bc460eabead6b
Reme Le Hane <reme@loop.co.za>
2023-04-19 08:58:49 +0200

    test(drivers): [LOOP] testing driver display widget

--- apps/frontend/pages/dashboard/components/Drivers/actions.ts
+++ apps/frontend/pages/dashboard/components/Drivers/actions.ts
@@ -16,7 +16,38 @@ const Actions = (_state, _setState) => ({
       idle_since: document.idle_since,
       created_at: document.created_at
     }))
+  },
+  stringAvatar: (name: string) => {
+    const fn = name.split(' ')[0][0]
+    const ln = name.split(' ')[1] ? name.split(' ')[1][0] : ''
+
+    return {
+      sx: {
+        bgcolor: _stringToColor(name)
+      },
+      children: `${fn}${ln}`
+    }
   }
 })
+
+function _stringToColor(str: string) {
+  let hash = 0
+  let i
+
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < str.length; i += 1) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash)
+  }
+
+  let color = '#'
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff
+    color += `00${value.toString(16)}`.slice(-2)
+  }
+  /* eslint-enable no-bitwise */
+
+  return color
+}
 
 export default Actions
