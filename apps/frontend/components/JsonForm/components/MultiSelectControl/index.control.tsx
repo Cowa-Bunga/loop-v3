93949ec66b916775bf0eaab99ceb3a5bb49c0807
Reme Le Hane <reme@loop.co.za>
2023-05-04 15:57:25 +0200

    feat(crate_task): [LOOP] setting up custom controller

---
+++ apps/frontend/components/JsonForm/components/MultiSelectControl/index.control.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+import { withJsonFormsControlProps } from '@jsonforms/react'
+import InputLabel from '@mui/material/InputLabel'
+import FormControl from '@mui/material/FormControl'
+import Select, { SelectChangeEvent } from '@mui/material/Select'
+import { Checkbox, ListItemText, MenuItem } from '@mui/material'
+import { JsonSchema } from '@jsonforms/core'
+
+interface IMultiSelectControl {
+  data: any
+  path: string
+  label: string
+  schema: JsonSchema
+
+  handleChange(path: string, value: any): void
+}
+
+const ITEM_HEIGHT = 48
+const ITEM_PADDING_TOP = 8
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250
+    }
+  }
+}
+
+const MultiSelectControl = ({
+  // the incoming selected items
+  data = [],
+  handleChange,
+  path,
+  ...rest
+}: IMultiSelectControl) => {
+  const [selected, setSelected] = useState<string[]>([])
+
+  const onChange = (event: SelectChangeEvent<typeof selected>) => {
+    const {
+      target: { value }
+    } = event
+    setSelected(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value
+    )
+
+    handleChange(path, selected)
+  }
+
+  return (
+    <>
+      <FormControl sx={{ width: '100%' }}>
+        <InputLabel id="demo-multiple-checkbox-label">{rest.label}</InputLabel>
+        <Select
+          labelId="demo-multiple-checkbox-label"
+          id="demo-multiple-checkbox"
+          multiple
+          value={selected}
+          onChange={onChange}
+          renderValue={(selected) => selected.join(', ')}
+          MenuProps={MenuProps}
+          label={rest.label}
+        >
+          {rest.schema.enum.map((v: string) => (
+            <MenuItem key={v} value={v}>
+              <Checkbox checked={selected.includes(v)} />
+              <ListItemText primary={v} />
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </>
+  )
+}
+
+export default withJsonFormsControlProps(MultiSelectControl)
---
+++ apps/frontend/components/JsonForm/components/MultiSelectControl/index.tester.ts
@@ -0,0 +1,3 @@
+import { rankWith, scopeEndsWith } from '@jsonforms/core'
+
+export default rankWith(5, scopeEndsWith('abandonFlow'))
