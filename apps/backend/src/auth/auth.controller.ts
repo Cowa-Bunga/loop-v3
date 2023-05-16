88b5a1a7700442ebad3d53c12988bae2fbed7d82
Mas'ood Gool <masood@loop.co.za>
2023-05-16 11:12:40 +0200

    LOOP-2500 create trips endpoint

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -1,8 +1,9 @@
 import { Controller } from '@nestjs/common'
 import { AuthService } from './auth.service'
-import { ApiSecurity, ApiTags } from '@nestjs/swagger'
+import { ApiSecurity, ApiTags, ApiForbiddenResponse } from '@nestjs/swagger'
 
 @ApiTags('Auth')
+@ApiForbiddenResponse({ description: 'Unauthorized Request' })
 @ApiSecurity('x-api-key')
 @Controller('auth')
 export class AuthController {
--- apps/backend/src/trip/trip.controller.ts
+++ apps/backend/src/trip/trip.controller.ts
@@ -5,19 +5,49 @@ import {
   Param,
   ParseArrayPipe,
   Query,
-  Request
+  Request,
+  HttpCode,
+  HttpStatus,
+  Post
 } from '@nestjs/common'
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiSecurity
+} from '@nestjs/swagger'
 import { TripService } from './trip.service'
+import { CreateTripDto } from './dto/create-trip.dto'
 import { GetTripDto } from './dto/get-trip.dto'
-import { ApiTags, ApiSecurity} from '@nestjs/swagger'
+//import { AcceptAdhocTripDto } from './dto/accept-adhoc-trip.dto'
+//import { AcceptAdhocTripService } from './accept-adhoc-trip.service'
 
 @ApiTags('Trips')
 @Controller('trip')
 @ApiSecurity('x-api-key')
 export class TripController {
-  constructor(private readonly tripService: TripService) {}
+  constructor(
+    private readonly tripService: TripService,
+    //private readonly acceptAdhocTripService: AcceptAdhocTripService
+  ) {}
+
+  @Get(':trip_id')
+  @ApiOperation({ summary: 'Get trip details by trip_id' })
+  @ApiOkResponse({ description: 'The resource was returned successfully' })
+  @ApiBadRequestResponse({ description: 'Invalid input data provided.' })
+  async getTrip(@Param('trip_id') trip_id: string, @Request() req) {
+    const client_id = req.client
+    const getTripDto: GetTripDto = { trip_id }
+    return await this.tripService.getTrip(getTripDto, client_id)
+  }
 
   @Get()
+  @ApiOperation({ summary: 'Get multiple trips by trip_ids' })
+  @ApiOkResponse({ description: 'The resource was returned successfully' })
+  @ApiBadRequestResponse({ description: 'Invalid input data provided.' })
   async getTrips(
     @Query('trip_ids', ParseArrayPipe) trip_ids: string[],
     @Request() req
@@ -26,11 +56,31 @@ export class TripController {
     return await this.tripService.getTrips(trip_ids, client_id)
   }
 
-  @Get(':trip_id')
-  async getTrip(@Param('trip_id') trip_id: string, @Request() req) {
+  @Post("/create")
+  @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({ summary: 'Create a new trip' })
+  @ApiCreatedResponse({ description: 'The trip has been successfully created.' })
+  @ApiBadRequestResponse({ description: 'Invalid input data provided.' })
+  @ApiBody({ type: CreateTripDto })
+  async createTrip(@Body() createTripDto: CreateTripDto, @Request() req) {
     const client_id = req.client
-    const getTripDto: GetTripDto = { trip_id } // Create a new GetTripDto object
-    return await this.tripService.getTrip(getTripDto, client_id)
+    return await this.tripService.createTrip(createTripDto, client_id)
   }
 
+  // @Post('accept-adhoc')
+  // @HttpCode(HttpStatus.CREATED)
+  // @ApiCreatedResponse({
+  //   description: 'Ad-hoc trip accepted successfully',
+  //   type: AcceptAdhocTripDto
+  // })
+  // @ApiBadRequestResponse({ description: 'Invalid input data provided.' })
+  // @ApiOperation({ summary: 'Add an order to an active trip' })
+  // @ApiBody({ type: AcceptAdhocTripDto })
+  // async acceptAdhocTrip(
+  //   @Body() acceptAdhocTrip: AcceptAdhocTripDto, 
+  //   @Request() req
+  // ) {
+  //   const client_id = req.client
+  //   return await this.acceptAdhocTripService.acceptAdhocTrip(acceptAdhocTrip, client_id)
+  // }
 }
