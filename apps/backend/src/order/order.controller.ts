8e0ea66a977289acc2b7fc5cc05d574781fde03a
matthew-loop <matthew@loop.co.za>
2023-05-16 16:46:37 +0200

    feat(order): [LOOP-2575] Updated order service to make use of custom client decorator

--- apps/backend/src/order/order.controller.ts
+++ apps/backend/src/order/order.controller.ts
@@ -1,16 +1,9 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  ParseArrayPipe,
-  Post,
-  Query,
-  Request
-} from '@nestjs/common'
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common'
 import { OrderService } from './order.service'
 import { CreateOrderDto } from './dto/create-order.dto'
 import { ApiTags } from '@nestjs/swagger'
+import { Client } from '../shared/decorators/client.decorator'
+import { ClientRequest } from '../shared/entities/request.entity'
 
 @ApiTags('Orders')
 @Controller('order')
@@ -19,22 +12,34 @@ export class OrderController {
 
   @Get()
   async getOrders(
-    @Query('order_ids', ParseArrayPipe) order_ids: string[],
-    @Request() req
+    @Client() client: ClientRequest,
+    @Query('order_ids') order_ids?: string[]
   ) {
-    const client_id = req.client
-    return await this.orderService.getOrders(order_ids, client_id)
+    const client_id = client.id
+    let orders
+    if (order_ids && order_ids.length > 0) {
+      orders = await this.orderService.getOrders(order_ids, client_id)
+    } else {
+      orders = await this.orderService.getAllOrders(client_id)
+    }
+
+    return orders
   }
 
   @Get(':order_id')
-  async getOrder(@Param('order_id') order_id: string, @Request() req) {
-    const client_id = req.client
-    return await this.orderService.getOrder(order_id, client_id)
+  async getOrder(
+    @Param('order_id') order_id: string,
+    @Client() client: ClientRequest
+  ) {
+    return await this.orderService.getOrder(order_id, client.id)
   }
 
   @Post('/create')
-  async createOrder(@Body() createOrderDto: CreateOrderDto, @Request() req) {
-    const client_id = req.client
+  async createOrder(
+    @Body() createOrderDto: CreateOrderDto,
+    @Client() client: ClientRequest
+  ) {
+    const client_id = client.id
     return await this.orderService.createOrder(createOrderDto, client_id)
   }
 }
