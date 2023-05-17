2322fb1b7f5a92601c5f7a722ce134016f1de147
matthew-loop <matthew@loop.co.za>
2023-05-17 18:13:50 +0200

    chore(modules): Remove unused services in parent directory

--- apps/backend/src/route/route.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common'
-import { RouteService } from './services/route.service'
-import { RouteController } from './controllers/route.controller'
-
-@Module({
-  controllers: [RouteController],
-  providers: [RouteService]
-})
-export class RouteModule {}
