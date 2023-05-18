0508ab85c9991dfbcfa641b3fcb3c382711f05d6
matthew-loop <matthew@loop.co.za>
2023-05-18 14:00:59 +0200

    feat(client): [LOOP-2500] Added client service scaffolding

--- apps/backend/src/modules/main.module.ts
+++ apps/backend/src/modules/main.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module'
 import { RouteModule } from './route/route.module'
 import { APP_GUARD } from '@nestjs/core'
 import { AuthGuard } from '../shared/guards/auth.guard'
+import { ClientModule } from './client/client.module'
 
 @Module({
-  imports: [OrderModule, AuthModule, TripModule, RouteModule],
+  imports: [OrderModule, AuthModule, TripModule, RouteModule, ClientModule],
   providers: [
     {
       provide: APP_GUARD,
