6c521428a459b9af76c9703f3bc828bb8ebcc256
matthew-loop <matthew@loop.co.za>
2023-05-18 14:38:19 +0200

    feat(auth): [LOOP-2500] Updated auth guard to include jwt auth

--- apps/backend/src/shared/guards/auth.guard.ts
+++ apps/backend/src/shared/guards/auth.guard.ts
@@ -1,29 +1,75 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common'
-import { ClientRequest } from '../entities/request.entity'
-import * as admin from 'firebase-admin'
+import { CanActivate, ExecutionContext, Injectable, Logger, UnauthorizedException } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { ConfigService } from '@nestjs/config'
+import { Request } from 'express'
+import { ClientRequest, UserRequest } from '../entities/request.entity'
+import { ClientService } from '../../modules/client/client.service'
+import { UserService } from '../../modules/user/user.service'
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(
+    private jwtService: JwtService,
+    private configService: ConfigService,
+    private clientService: ClientService,
+    private userService: UserService
+  ) {}
+
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest()
-    const apiKey = request.headers['x-api-key']
+    const apiKey = request.header('x-api-key')
+    const authHeader = request.header('Authorization')
 
-    if (!apiKey) {
-      throw new UnauthorizedException('Invalid or missing API Key.')
+    if (!apiKey && !authHeader) {
+      throw new UnauthorizedException('User is not authorized. Please provide valid authentication credentials.')
     }
     try {
-      const db = admin.firestore()
-      const clients = await db.collection('clients').where('api.key', '==', apiKey).limit(1).get()
-
-      if (clients.empty) {
-        throw new UnauthorizedException('Invalid or missing API Key.')
+      if (apiKey) {
+        await this.validateAPIKey(apiKey, request)
+      } else {
+        await this.validateToken(authHeader, request)
       }
-
-      const client = clients.docs.pop()
-      request['client'] = new ClientRequest(client)
     } catch {
-      throw new UnauthorizedException('Invalid or missing API Key.')
+      throw new UnauthorizedException('User is not authorized. Please provide valid authentication credentials.')
     }
     return true
+  }
+
+  validateAPIKey = async (apiKey: string, request: Request) => {
+    const clients = await this.clientService.getClientByKey(apiKey)
+
+    if (clients.empty) {
+      throw new UnauthorizedException('Invalid or missing API Key.')
+    }
+
+    const client = clients.docs.pop()
+    request['client'] = new ClientRequest(client)
+  }
+
+  validateToken = async (header: string, request: Request) => {
+    const [type, token] = header.split(' ') ?? []
+    if (type !== 'Bearer') {
+      throw new UnauthorizedException('User is not authorized. Please provide valid authentication credentials.')
+    }
+    const payload = await this.jwtService.verifyAsync(token, {
+      secret: this.configService.get<string>('JWT_SECRET')
+    })
+    const client_id = payload.client.id
+    const user_id = payload.user.id
+
+    const client = await this.clientService.getClientDoc(client_id)
+    const user = await this.userService.getUserDoc(user_id)
+
+    if (!client.exists || !user.exists) {
+      throw new UnauthorizedException('Client or user does not exist. Please provide valid authentication credentials.')
+    }
+
+    if (client.data().archived === true) {
+      throw new UnauthorizedException(
+        'Your company no longer has access to the Loop platform. Please contact customer support for further assistance.'
+      )
+    }
+    request['client'] = new ClientRequest(client)
+    request['user'] = new UserRequest(user)
   }
 }
