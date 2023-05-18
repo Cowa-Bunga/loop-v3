8147d4a85ac99236527efffe2ebeae105c4a8a11
matthew-loop <matthew@loop.co.za>
2023-05-18 14:24:59 +0200

    feat(client): [LOOP-2500] Added client doc accessor function

--- apps/backend/src/modules/client/client.service.ts
+++ apps/backend/src/modules/client/client.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common'
+import * as admin from 'firebase-admin'
 
 @Injectable()
-export class ClientService {}
+export class ClientService {
+  async getClientDoc(client_id: string) {
+    const db = admin.firestore()
+    const client = await db.collection('clients').doc(client_id).get()
+
+    return client
+  }
+
+  async getClientByKey(apiKey: string) {
+    const db = admin.firestore()
+    const clients = await db.collection('clients').where('api.key', '==', apiKey).limit(1).get()
+    return clients
+  }
+}
