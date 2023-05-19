043edba6a5c726221206d158f41a4cd2caf200ee
matthew-loop <matthew@loop.co.za>
2023-05-19 12:28:42 +0200

    feat(hub): [LOOP-2500] Added get hubs function

--- apps/backend/src/modules/hub/hub.service.ts
+++ apps/backend/src/modules/hub/hub.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common'
+import * as admin from 'firebase-admin'
+import { DocumentReference } from '@google-cloud/firestore'
 
 @Injectable()
-export class HubService {}
+export class HubService {
+  /**
+   * Get hubs
+   * @function
+   * @param {string} client_id
+   * @param {string[]} order_ids
+   *
+   * @throws {Error}
+   * @returns {Promise}
+   */
+  async getHubs(hub_refs: DocumentReference[]): Promise<any> {
+    const db = admin.firestore()
+
+    if (hub_refs.length === 0) {
+      return []
+    }
+
+    const snapshot = await db.getAll(...hub_refs)
+
+    const hubs = snapshot.map((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      }
+    })
+    return hubs
+  }
+}
