ceee4149ce18dcc938affd4d6cfb60ea90a21808
Mas'ood Gool <masood@loop.co.za>
2023-05-19 11:29:08 +0200

    Use custom decorators

--- apps/backend/src/modules/cluster/cluster.controller.ts
+++ apps/backend/src/modules/cluster/cluster.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Get, Param } from '@nestjs/common'
-import { ApiBadRequestResponse, ApiOkResponse, ApiOperation, ApiTags, ApiSecurity } from '@nestjs/swagger'
+import { ApiTags, ApiSecurity } from '@nestjs/swagger'
 import { Client } from '../../shared/decorators/client.decorator'
 import { ClientRequest } from '../../shared/entities/request.entity'
 import { ClusterService } from './cluster.service'
 import { ClusterDto } from './dto/cluster.dto'
+import { ApiGetRequest, ApiGetOneRequest } from '../../shared/decorators/api.decorator' // Import the decorators
 
 @ApiTags('Clusters')
 @Controller('cluster')
@@ -12,19 +13,15 @@ export class ClusterController {
   constructor(private readonly clusterService: ClusterService) {}
 
   @Get(':cluster_id')
-  @ApiOperation({ summary: 'Get cluster details by cluster_id' })
-  @ApiOkResponse({ description: 'The resource was returned successfully' })
-  @ApiBadRequestResponse({ description: 'Invalid input data provided.' })
+  @ApiGetOneRequest('cluster', { operation_content: 'Get cluster details by cluster_id' }) // Apply the decorator
   async getCluster(@Param('cluster_id') clusterId: string, @Client() client: ClientRequest) {
     const getClusterDto: ClusterDto = { clusterId }
     const client_id = client.id
-    return await this.clusterService.getCluster(getClusterDto,client_id)
+    return await this.clusterService.getCluster(getClusterDto, client_id)
   }
 
   @Get('/branch/:branch_id')
-  @ApiOperation({ summary: 'Get clusters by branch_id' })
-  @ApiOkResponse({ description: 'The resource was returned successfully' })
-  @ApiBadRequestResponse({ description: 'Invalid input data provided.' })
+  @ApiGetRequest('clusters', { operation_content: 'Get clusters by branch_id' }) // Apply the decorator
   async getClustersByBranch(@Param('branch_id') branchId: string, @Client() client: ClientRequest) {
     const client_id = client.id
     return this.clusterService.getClustersByBranch(client_id, branchId)
--- apps/backend/src/modules/trip/trip.controller.ts
+++ apps/backend/src/modules/trip/trip.controller.ts
@@ -1,40 +1,19 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  ParseArrayPipe,
-  Query,
-  HttpCode,
-  HttpStatus,
-  Post
-} from '@nestjs/common'
-import {
-  ApiBadRequestResponse,
-  ApiBody,
-  ApiCreatedResponse,
-  ApiOkResponse,
-  ApiOperation,
-  ApiTags,
-  ApiSecurity
-} from '@nestjs/swagger'
+import { Body, Controller, Get, Param, ParseArrayPipe, Query, HttpCode, HttpStatus, Post } from '@nestjs/common'
+import { ApiTags, ApiSecurity } from '@nestjs/swagger'
 import { Client } from '../../shared/decorators/client.decorator'
 import { ClientRequest } from '../../shared/entities/request.entity'
 import { TripService } from './trip.service'
 import { GetTripDto, CreateTripDto, AcceptAdhocTripDto } from './dto/trip.dto'
+import { ApiGetRequest, ApiGetOneRequest, ApiPostRequest } from '../../shared/decorators/api.decorator' // Import the decorators
 
 @ApiTags('Trips')
 @Controller('trip')
 @ApiSecurity('x-api-key')
 export class TripController {
-  constructor(
-    private readonly tripService: TripService,
-  ) {}
+  constructor(private readonly tripService: TripService) {}
 
   @Get(':trip_id')
-  @ApiOperation({ summary: 'Get trip details by trip_id' })
-  @ApiOkResponse({ description: 'The resource was returned successfully' })
-  @ApiBadRequestResponse({ description: 'Invalid input data provided.' })
+  @ApiGetOneRequest('trip', { operation_content: 'Get trip details by trip_id' }) // Apply the decorator
   async getTrip(@Param('trip_id') trip_id: string, @Client() client: ClientRequest) {
     const client_id = client.id
     const getTripDto: GetTripDto = { trip_id }
@@ -42,9 +21,7 @@ export class TripController {
   }
 
   @Get()
-  @ApiOperation({ summary: 'Get multiple trips by trip_ids' })
-  @ApiOkResponse({ description: 'The resource was returned successfully' })
-  @ApiBadRequestResponse({ description: 'Invalid input data provided.' })
+  @ApiGetRequest('trips', { operation_content: 'Get multiple trips by trip_ids' }) // Apply the decorator
   async getTrips(
     @Query('trip_ids', ParseArrayPipe) trip_ids: string[],
     @Client() client: ClientRequest
@@ -53,12 +30,9 @@ export class TripController {
     return await this.tripService.getTrips(trip_ids, client_id)
   }
 
-  @Post("/create")
+  @Post('/create')
   @HttpCode(HttpStatus.CREATED)
-  @ApiOperation({ summary: 'Create a new trip' })
-  @ApiCreatedResponse({ description: 'The trip has been successfully created.' })
-  @ApiBadRequestResponse({ description: 'Invalid input data provided.' })
-  @ApiBody({ type: CreateTripDto })
+  @ApiPostRequest('trip', CreateTripDto, { operation_content: 'Create a new trip' }) // Apply the decorator
   async createTrip(@Body() createTripDto: CreateTripDto, @Client() client: ClientRequest) {
     const client_id = client.id
     return await this.tripService.createTrip(createTripDto, client_id)
@@ -66,16 +40,9 @@ export class TripController {
 
   @Post('accept-adhoc')
   @HttpCode(HttpStatus.CREATED)
-  @ApiCreatedResponse({
-    description: 'Ad-hoc trip accepted successfully',
-    type: AcceptAdhocTripDto
-  })
-  @ApiBadRequestResponse({ description: 'Invalid input data provided.' })
-  @ApiOperation({ summary: 'Add an order to an active trip' })
-  @ApiBody({ type: AcceptAdhocTripDto })
-  async acceptAdhocTrip(
-    @Body() acceptAdhocTrip: AcceptAdhocTripDto, @Client() client: ClientRequest) {
-      const client_id = client
+  @ApiPostRequest('trip', AcceptAdhocTripDto, { operation_content: 'Add an order to an active trip' }) // Apply the decorator
+  async acceptAdhocTrip(@Body() acceptAdhocTrip: AcceptAdhocTripDto, @Client() client: ClientRequest) {
+    const client_id = client
     return await this.acceptAdhocTrip(acceptAdhocTrip, client_id)
   }
 }
