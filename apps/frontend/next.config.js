0ca07406e1de5e7a391a2a913cf1d55aa7d6218f
timLycett-loop <128364653+timLycett-loop@users.noreply.github.com>
2023-05-25 15:47:31 +0200

    made sentry optional

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -21,43 +21,24 @@ const nextConfig = {
   //     fallback: [
   //       {
   //         source: '/api/:path*',
-  //         destination: 'http://localhost:3333/api/:path*'
+  //         destination: `${process.env.API_URL}/api/:path*`
   //       }
   //     ]
   //   }
   // }
 
-  // Optional build-time configuration options
   sentry: {
-    // See the sections below for information on the following options:
-    //   'Configure Source Maps':
-    //     - disableServerWebpackPlugin
-    //     - disableClientWebpackPlugin
     hideSourceMaps: process.env.NODE_ENV === 'production'
-    //     - widenClientFileUpload
-    //   'Configure Legacy Browser Support':
-    //     - transpileClientSDK
-    //   'Configure Serverside Auto-instrumentation':
-    //     - autoInstrumentServerFunctions
-    //     - excludeServerRoutes
-    //   'Configure Tunneling to avoid Ad-Blockers':
-    //     - tunnelRoute
   }
 }
 
+// https://github.com/getsentry/sentry-webpack-plugin#options.
 const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
   org: 'loop-platform-pty-ltd',
   project: 'loop-pro-frontend',
   silent: true
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
 }
 
-module.exports = withNx(
-  withSentryConfig(nextConfig, sentryWebpackPluginOptions)
-)
+const exportConfigs = process.env.SENTRY_ENABLED ? withSentryConfig(nextConfig, sentryWebpackPluginOptions) : nextConfig
+
+module.exports = withNx(exportConfigs)
