bcfc29fe2fc8061e4e67533b3fc090f3fe29649b
matthew-loop <matthew@loop.co.za>
2023-05-25 12:45:29 +0200

    feat(dashboard): [LOOP-2500] Code cleanup

--- apps/backend/src/modules/dashboard/dashboard.service.ts
+++ apps/backend/src/modules/dashboard/dashboard.service.ts
@@ -1,13 +1,14 @@
-import { BadRequestException, Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable, Logger } from '@nestjs/common'
 import * as admin from 'firebase-admin'
 import { ClientRequest, UserRequest } from '../../shared/entities/request.entity'
 import { HubService } from '../hub/hub.service'
 import { BranchService } from '../branch/branch.service'
 import { EssentialBranch } from '../branch/entities/branch.entity'
-import { EssentialHub } from '../hub/entities/hub.entity'
+import { EssentialHub, Hub } from '../hub/entities/hub.entity'
 import { OrderService } from '../order/order.service'
 import { DriverService } from '../driver/driver.service'
 import { EssentialDriver } from '../driver/entities/driver.entity'
+import { EssentialOrder } from '../order/entities/order.entity'
 
 @Injectable()
 export class DashboardService {
@@ -22,29 +23,36 @@ export class DashboardService {
     if (user.hub_refs.length === 0) {
       throw new BadRequestException('User has access to no hubs')
     }
-    /**
-     * 1. Loop through hubs
-     * 2. Get All branches for each hub
-     * 3. Get all orders for each branch
-     */
+    const hubData = await Promise.all(
+      user.hub_refs.map(async (ref) => {
+        const hubPromise = this.hubService.getHub(ref)
+        const driverPromise = this.driverService.getDriversForHub(ref.id, client.id)
+        const branchDocs = await this.branchService.getBranchesForHub(ref, client.id)
+        const branches = branchDocs.map((doc) => new EssentialBranch(doc))
 
-    const hubData = user.hub_refs.map(async (ref) => {
-      const hub: EssentialHub = await this.hubService.getHub(ref, true)
-      const branches: EssentialBranch[] = await this.branchService.getBranchesForHub(ref, client.id, true)
-      const drivers: EssentialDriver[] = await this.driverService.getDriversForHub(ref.id, client.id, true)
-      await Promise.all(
-        branches.map(async (branch) => {
-          const orders = await this.orderService.getOrdersForBranch(branch.id, client.id, true)
-          branch.setOrders(orders)
-        })
-      )
-      return {
-        hub,
-        branches,
-        drivers
-      }
-    })
-    const data = await Promise.all(hubData)
+        // Loop through all branch documents and retrieve orders for branch
+        await Promise.all(
+          branches.map(async (branch) => {
+            const orderDocs = await this.orderService.getOrdersForBranch(branch.id, client)
+            const orders = orderDocs.map((doc) => new EssentialOrder(doc))
+            branch.setOrders(orders)
+          })
+        )
+
+        const [hubDoc, driverDocs] = await Promise.all([hubPromise, driverPromise])
+        const drivers = driverDocs.map((doc) => new EssentialDriver(doc))
+        const hub = new EssentialHub(hubDoc)
+
+        return {
+          ...hub,
+          branches,
+          drivers
+        }
+      })
+    )
+
+    const data = { hubs: hubData }
+
     return data
   }
 }
