686fc8ac6a95063fd9a785501ee5ba1930a2570c
timLycett-loop <128364653+timLycett-loop@users.noreply.github.com>
2023-06-06 15:39:22 +0200

    codefix

--- apps/frontend/components/TimeLine3/index.tsx
+++ apps/frontend/components/TimeLine3/index.tsx
@@ -3,11 +3,7 @@ import { mock } from './mock'
 
 export default function TimelineGantt({ data }) {
   return (
-    <div
-      style={{
-        height: '300px'
-      }}
-    >
+    <div style={{ height: '300px' }}>
       <Gantt>
         <Gantt.Controls />
         <Gantt.Chart data={mock} style={{ background: 'red' }} />
