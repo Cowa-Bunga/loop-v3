0e18b996824e12e727cad3c1d7e8a3da027152d7
matthew-loop <matthew@loop.co.za>
2023-06-08 13:58:51 +0200

    feat(trip): [LOOP-2699] Trip module cleanup

--- apps/public-api/src/modules/trip/trip.module.ts
+++ apps/public-api/src/modules/trip/trip.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
 import { TripService } from './trip.service'
 import { TripController } from './trip.controller'
+import { DriverModule } from '../driver/driver.module'
 //import { AcceptAdhocTripService } from './accept-adhoc-trip.service'
 
-
 @Module({
+  imports: [DriverModule],
   controllers: [TripController],
   providers: [TripService],
   exports: [TripService]
