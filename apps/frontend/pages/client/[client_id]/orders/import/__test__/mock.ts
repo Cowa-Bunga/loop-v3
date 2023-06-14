b3191fe2502c5a581f6a631efa68e718e2f194aa
Mas'ood Gool <masood@loop.co.za>
2023-06-14 11:01:56 +0200

    init imports page

---
+++ apps/frontend/pages/client/[client_id]/orders/import/__test__/mock.ts
@@ -0,0 +1,35 @@
+export const rows = [
+  {
+    id: 1,
+    col1: 'today',
+    col2: '2',
+    col3: '1',
+    col4: '1',
+    col5: 'MAsood',
+  },
+  {
+    id: 2,
+    col1: 'tomorrow',
+    col2: '20',
+    col3: '10',
+    col4: '10',
+    col5: 'Tony Stank',
+  },
+  {
+    id: 3,
+    col1: 'together',
+    col2: '90',
+    col3: '90',
+    col4: '0',
+    col5: 'Monkey D Luffy',
+  }
+]
+
+export const columns = [
+  { field: 'col1', headerName: 'Date', minWidth: 288 },
+  { field: 'col2', headerName: 'Total Orders', minWidth: 288 },
+  { field: 'col3', headerName: 'Succeeded', minWidth: 288 },
+  { field: 'col4', headerName: 'Failed', minWidth: 288 },
+  { field: 'col5', headerName: 'Imported By', minWidth: 288 },
+  { field: 'actions', headerName: '', minWidth: 200 }
+]
--- apps/frontend/pages/client/[client_id]/orders/import/index.page.tsx
+++ apps/frontend/pages/client/[client_id]/orders/import/index.page.tsx
@@ -1,7 +1,58 @@
 import { LayoutBase } from '@components'
+import Button from '@mui/material/Button'
+import { DataGrid } from '@mui/x-data-grid'
+import { rows, columns } from './__test__/mock'
+import { Tabs, Tab, Divider, Box, Typography } from '@mui/material'
+import { positions } from '@mui/system'
+
 
 const OrdersImport = () => {
-  return <LayoutBase>importOrders</LayoutBase>
+  return <LayoutBase>
+    
+    <Button onClick={null}>O   Home</Button>
+    <br/>
+    <Box sx={{
+      display: 'inline-flex',
+        px: 5,
+        py: 5
+    }}>
+        <Box>
+          <Typography variant="h5">
+            <b>Import Orders</b>
+          </Typography>
+          <br />
+        </Box>
+        <Button sx={{
+          py:0,
+          my:0,
+          left: '85%'
+          }}>Export import Template X</Button>
+    </Box>
+    <Box sx={{
+        px: 5,
+        py: 5,
+        backgroundColor: 'lightgray',
+        '&:hover': {
+          backgroundColor: 'primary.main',
+          opacity: [0.9, 0.8, 0.7],
+        },
+      }}>
+    <Button variant='contained'>Import Orders</Button>
+    <Button variant='contained'>Create Trip</Button>
+    <Box sx={{
+        px: 5,
+        py: 5,
+        backgroundColor: 'white',
+        '&:hover': {
+          backgroundColor: 'primary.main',
+          opacity: [0.9, 0.8, 0.7],
+        },
+      }}>
+    <DataGrid rows={rows} columns={columns} autoHeight hideFooter density="comfortable" />
+    </Box>
+    </Box>
+    
+    </LayoutBase>
 }
 
 export default OrdersImport
